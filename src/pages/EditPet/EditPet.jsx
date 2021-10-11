9285f18fe1f9d415a2a7bf1246c0f3fd0e5b5510
sebasgp <sebastian.gallegop@udea.edu.co>
2021-10-10 20:32:40 -0500

    Styles editpet page

--- src/pages/EditPet/EditPet.jsx
+++ src/pages/EditPet/EditPet.jsx
@@ -1,64 +1,64 @@
-import React,{useState, useEffect} from 'react'
-import {getPet, editPet} from "../../services/pets"
-import {useParams, Link} from "react-router-dom"
-import "./editPet.css"
+import React, { useState, useEffect } from "react";
+import { getPet, editPet } from "../../services/pets";
+import { useParams, Link } from "react-router-dom";
+import "./editPet.css";
 
 function EditPet() {
-
-  const {id}=useParams()
-
+  const { id } = useParams();
 
   const [inputData, setInputData] = useState({
-    name:"",
-    size:"",
-    cares:""
-  })
+    name: "",
+    size: "",
+    cares: "",
+  });
 
   useEffect(() => {
-    getPet(id)
-    .then(data=>setInputData(data))
-  }, [])
+    getPet(id).then((data) => setInputData(data));
+  }, []);
 
-  const  [petEdit, setPetEdit]= useState({})
+  const [petEdit, setPetEdit] = useState({});
 
-  const handleSubmit=async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setPetEdit(inputData)
- 
-    const res =await editPet(id, inputData);
-    console.log(res)
-    window.location ="/" 
-    
-  }
+    setPetEdit(inputData);
 
-  const handleInputs=(e)=>{
-    setInputData({...inputData,[e.target.name]:e.target.value})
-  }
+    const res = await editPet(id, inputData);
+    console.log(res);
+    window.location = "/";
+  };
 
+  const handleInputs = (e) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
+  };
 
   return (
-    <div className="editContainer">
-       <form onSubmit={handleSubmit} className="editForm" >
-          <div className="inputs-container">
+    <div className="editPetContainer">
+      <Link to="/" className="back-btn">
+        Volver ⬅
+      </Link>
+      <div className="editPet">
+        <h2 className="editPet-title">Actualizar Mascota</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="inputs-container-editPet">
             <input
-              className="input-createPet"
+              className="input-editPet"
               type="text"
               name="name"
               onChange={handleInputs}
               placeholder="Nombre Mascota"
               value={inputData.name}
             />
-           
+
             <input
-              className="input-createPet"
+              className="input-editPet"
               type="text"
               name="size"
               onChange={handleInputs}
               placeholder="Tamaño"
               value={inputData.size}
             />
-            <input
-              className="input-createPet"
+            <textarea
+              className="input-editPet"
               type="text"
               name="cares"
               onChange={handleInputs}
@@ -68,9 +68,9 @@ function EditPet() {
           </div>
           <button className="btn-create">Editar</button>
         </form>
-        <Link to="/">Volver</Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default EditPet
+export default EditPet;
